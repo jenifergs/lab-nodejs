e4e4a4d7c6a3fe36adc1de2d50aecda47f9bff63
jenifergs <contato.comercialjenifer@gmail.com>
2022-12-16 10:28:41 -0300

    feat: Create and test new notification

---
+++ test/repositories/in-memory-notifications-repository.ts
@@ -0,0 +1,11 @@
+import { NotificationsRepository } from 'src/aplication/repositories/notifications-repository';
+import { Notification } from 'src/aplication/entities/notification';
+export class InMemoryNotificationsRepository
+  implements NotificationsRepository
+{
+  public notifications: Notification[] = [];
+
+  async create(notification: Notification) {
+    this.notifications.push(notification);
+  }
+}
