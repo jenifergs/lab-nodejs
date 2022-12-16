b18161907d332660939b39bfdf6a7b2a8b6e3011
jenifergs <contato.comercialjenifer@gmail.com>
2022-12-16 10:39:15 -0300

    feat: In memory database

--- src/aplication/use-cases/send-notification.spec.ts
+++ src/aplication/use-cases/send-notification.spec.ts
@@ -1,24 +1,17 @@
-import { Notification } from '../entities/notification';
+import { InMemoryNotificationsRepository } from '../../../test/repositories/in-memory-notifications-repository';
 import { SendNotification } from './send-notification';
 
-const notifications: Notification[] = [];
-
-const notificationRepository = {
-  async create(notification: Notification) {
-    notifications.push(notification);
-  },
-};
 describe('send notification', () => {
   it('should be able  to send a notification', async () => {
+    const notificationRepository = new InMemoryNotificationsRepository();
     const sendNotification = new SendNotification(notificationRepository);
 
-    await sendNotification.execute({
+    const { notification } = await sendNotification.execute({
       content: 'Thisd is a notification',
       category: 'social',
       recipientId: 'example-recipient-id',
     });
 
-    console.log(notifications);
-    expect(notifications).toHaveLength(1);
+    expect(notificationRepository.notifications[0]).toEqual(notification);
   });
 });
--- test/repositories/in-memory-notifications-repository.ts
+++ test/repositories/in-memory-notifications-repository.ts
@@ -1,5 +1,6 @@
-import { NotificationsRepository } from 'src/aplication/repositories/notifications-repository';
+import { NotificationsRepository } from '../../src/aplication/repositories/notifications-repository';
 import { Notification } from 'src/aplication/entities/notification';
+
 export class InMemoryNotificationsRepository
   implements NotificationsRepository
 {
