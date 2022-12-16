e0b97fe8b5f1165e46c0fd2eb1e09929b7e7303c
jenifergs <contato.comercialjenifer@gmail.com>
2022-12-16 12:13:56 -0300

    feat:

---
+++ src/infra/database/prisma/repositories/prisma-notificatios-repository.ts
@@ -0,0 +1,19 @@
+import { Notification } from 'src/aplication/entities/notification';
+import { NotificationsRepository } from '../../../../aplication/repositories/notifications-repository';
+import { PrismaService } from '../prisma.service';
+
+export class PrismaNotificationRepository implements NotificationsRepository {
+  constructor(private prismaService: PrismaService) {}
+  async create(notification: Notification): Promise<void> {
+    await this.prismaService.notification.create({
+      data: {
+        id: notification.id,
+        content: notification.content.value,
+        category: notification.category,
+        recipientId: notification.recipientId,
+        readAt: notification.readAt,
+        createdAt: notification.createdAt,
+      },
+    });
+  }
+}
